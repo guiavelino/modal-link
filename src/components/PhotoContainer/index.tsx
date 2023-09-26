b632ef5fe59f9c98cf737276bddea3c26a0bb942
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2023-09-26 01:00:17 -0300

    feat: add PhotoCard Responsivity

--- src/components/PhotoContainer/index.tsx
+++ src/components/PhotoContainer/index.tsx
@@ -1,35 +1,37 @@
-import { title } from 'process';
-import styles from './styles.module.scss';
-import Image, { StaticImageData } from 'next/image';
-import Alert from '@mui/material/Alert';
+import Image, { StaticImageData } from "next/image";
+import Alert from "@mui/material/Alert";
+import { BiSolidCamera } from "react-icons/bi";
+
+import { title } from "process";
+
+import styles from "./styles.module.scss";
 
 type PhotoContainerProps = {
-    title: string;
-    exampleImage: StaticImageData;
-}
+  title: string;
+  exampleImage: StaticImageData;
+};
 
 export default function PhotoContainer({ title, exampleImage }: PhotoContainerProps) {
+  return (
+    <main className={styles.photoContainer}>
+      <div className={styles.photoCard}>
+        <h1>{title}</h1>
 
-    return (
-        <main className={styles.photoContainer}>
-            <h1>{title}</h1>
-            <Image
-                src={exampleImage}
-                width={332}
-                height={200}
-                alt=''
-                objectFit='fill'
-            />
-            <Alert className={styles.alertContainer} variant="outlined" severity="info" style={{ color: '#03a9f4' }}>
-                Fica a dica: tente fazer a foto na horizontal.
-            </Alert>
-            <h1>Exemplo</h1>
-            <Image
-                src={exampleImage}
-                width={332}
-                height={200}
-                alt=''
-            />
-        </main>
-    );
+        <div className={styles.picture}>
+          <button>
+            Tirar Foto <BiSolidCamera size={24} />
+          </button>
+        </div>
+      </div>
+
+      <Alert className={styles.alertContainer} variant="outlined" severity="info" style={{ color: "#03a9f4" }}>
+        Fica a dica: tente fazer a foto na horizontal.
+      </Alert>
+
+      <div className={styles.photoCard}>
+        <h1>Exemplo</h1>
+        <Image src={exampleImage} width={332} height={200} alt="" />
+      </div>
+    </main>
+  );
 }
