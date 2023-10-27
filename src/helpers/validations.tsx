4425a0a8eb9362a2e7bbeeee5d1ce80659b3b41f
Guilherme Avelino <guilhermeavelino1@outlook.com>
2023-10-27 15:34:59 -0300

    feat: validate transit board

--- src/helpers/validations.tsx
+++ src/helpers/validations.tsx
@@ -7,3 +7,8 @@ export const validateCPF = (cpf: string): boolean => {
     const regex = /^([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/
     return regex.test(cpf);
 }
+
+export const validateTransitBoard = (transitBoard: string): boolean  => {
+    const regex = /^[a-zA-Z]{3}[0-9][A-Za-z0-9][0-9]{2}$/
+    return regex.test(transitBoard);
+}
