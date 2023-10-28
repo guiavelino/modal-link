4c858c4b6df37b040257e8966d9f04869b11999c
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2023-10-28 00:32:38 -0300

    remove unnused react import in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { RequestModalProvider } from "@/contexts/RequestModal";
 import "@/styles/globals.scss";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import React from "react";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
